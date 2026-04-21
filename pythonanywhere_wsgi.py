218a021ba81bc7caaf1435f843e463cf5308858f
Islomov-Diyor <diyorbekshaxrisabz@gmail.com>
2026-04-21 14:06:40 +0500

    Set PythonAnywhere username DiyorbekIslomov in WSGI config

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -1,16 +1,15 @@
 import sys
 import os
 
-# Replace 'yourusername' with your actual PythonAnywhere username
-path = '/home/yourusername/DIFFPHYS'
+path = '/home/DiyorbekIslomov/DIFFPHYS'
 if path not in sys.path:
     sys.path.insert(0, path)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'diffphys.settings'
 os.environ['SECRET_KEY'] = 'replace-with-a-strong-secret-key'
 os.environ['DEBUG'] = 'False'
-os.environ['ALLOWED_HOSTS'] = 'yourusername.pythonanywhere.com'
-os.environ['HF_TOKEN'] = 'your-huggingface-token-here'
+os.environ['ALLOWED_HOSTS'] = 'DiyorbekIslomov.pythonanywhere.com'
+os.environ['HF_TOKEN'] = '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'
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
